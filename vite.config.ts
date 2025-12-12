88a9d890fba670c6f7d2de67688cf2f1599ef490
Roman Lee <romanlee1116@gmail.com>
2025-12-12 11:54:43 +0800

    feat: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/oldschool-website/',
+  build: {
+    outDir: 'docs',
+  },
+  publicDir: 'public'
 })
